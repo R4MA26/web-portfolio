89e8cfdeb23a9c9a120abd094e1ff562b2b4b825
R4MA26 <ramaismaya25@gmail.com>
2021-11-08 22:59:42 +0800

    web-portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// scroll up to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+
+// Nav hamburger selections 
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+// show
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+
+//tdk berfungsi
+// Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll("nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
